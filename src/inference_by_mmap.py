3c927c18df4e5e6d7cff9cb3a1d4141833f4caa7
EC-K <elxone.19jt@gmail.com>
2025-03-02 20:12:25 +0900

    style: changed the number of spaces when indenting (2 -> 4)

--- src/inference_by_mmap.py
+++ src/inference_by_mmap.py
@@ -11,54 +11,54 @@ from pathlib import Path
 import time
 
 def main_loop():
-  # Break in key Ctrl+C pressed
-  if cv2.waitKey(5) & 0xFF == 27:
-    return False
+    # Break in key Ctrl+C pressed
+    if cv2.waitKey(5) & 0xFF == 27:
+        return False
 
-  image = cv2.cvtColor(np_mmap, cv2.COLOR_BGRA2RGB)
-  holistic_detector.inference(image)
+    image = cv2.cvtColor(np_mmap, cv2.COLOR_BGRA2RGB)
+    holistic_detector.inference(image)
 
-  # Filtering (Note: use copy.deepcopy if you need to filter results)
-  results = holistic_detector.results
+    # Filtering (Note: use copy.deepcopy if you need to filter results)
+    results = holistic_detector.results
 
-  # Send results to solver app
-  packed_results = packer.pack_holistic_landmarks_result(results)
-  pose_sender.send_holistic_landmarks(packed_results)
+    # Send results to solver app
+    packed_results = packer.pack_holistic_landmarks_result(results)
+    pose_sender.send_holistic_landmarks(packed_results)
 
-  # Visualize resulted landmarks
-  annotated_image = image
-  if results.hand_landmarks is not None:
-    annotated_image = visualizer.draw_hand_landmarks_on_image(annotated_image, results.hand_landmarks)
-  if results.face_results is not None:
-    annotated_image = visualizer.draw_face_landmarks_on_image(annotated_image, results.face_results)
-  cv2.imshow('MediaPipe Landmarks', cv2.flip(cv2.cvtColor(annotated_image, cv2.COLOR_RGB2BGR), 1))
-  return True
+    # Visualize resulted landmarks
+    annotated_image = image
+    if results.hand_landmarks is not None:
+        annotated_image = visualizer.draw_hand_landmarks_on_image(annotated_image, results.hand_landmarks)
+    if results.face_results is not None:
+        annotated_image = visualizer.draw_face_landmarks_on_image(annotated_image, results.face_results)
+    cv2.imshow('MediaPipe Landmarks', cv2.flip(cv2.cvtColor(annotated_image, cv2.COLOR_RGB2BGR), 1))
+    return True
 
 if __name__ == "__main__":
-  mp_drawing = mp.solutions.drawing_utils
-  mp_drawing_styles = mp.solutions.drawing_styles
+    mp_drawing = mp.solutions.drawing_utils
+    mp_drawing_styles = mp.solutions.drawing_styles
 
-  pose_sender = HolisticPoseSender("localhost", 9001)
-  pose_sender.connect()
+    pose_sender = HolisticPoseSender("localhost", 9001)
+    pose_sender.connect()
 
-  holistic_detector = DetectorHandler()
+    holistic_detector = DetectorHandler()
 
-  width = 1280
-  height = 720
+    width = 1280
+    height = 720
 
-  project_root_directory = str(Path(__file__).parent.parent.parent)
-  filepath = project_root_directory + "/colorImg.dat"
+    project_root_directory = str(Path(__file__).parent.parent.parent)
+    filepath = project_root_directory + "/colorImg.dat"
 
-  np_mmap = np.memmap(
-      filepath,
-      dtype='uint8',
-      mode='r',
-      shape=(height, width, 4),
-  )
+    np_mmap = np.memmap(
+        filepath,
+        dtype='uint8',
+        mode='r',
+        shape=(height, width, 4),
+    )
 
-  doLoop = True
-  while doLoop:
-    doLoop = main_loop()
-    time.sleep(1/60)
+    doLoop = True
+    while doLoop:
+        doLoop = main_loop()
+        time.sleep(1/60)
 
-  cv2.destroyAllWindows()
+    cv2.destroyAllWindows()
--- src/inference_by_webcam.py
+++ src/inference_by_webcam.py
@@ -11,46 +11,46 @@ from pathlib import Path
 
 
 if __name__ == "__main__":
-  pose_sender = HolisticPoseSender()
-  pose_sender.connect()
+    pose_sender = HolisticPoseSender()
+    pose_sender.connect()
 
-  root_directory = str(Path(__file__).parent.parent)
-  holistic_detector = DetectorHandler(
-    pose=PoseDetector(root_directory + "/models/pose_landmarker_full.task"),
-    hand=HandDetector(root_directory + "/models/hand_landmarker.task"),
-    face=FaceDetector(root_directory + "/models/face_landmarker.task")
-  )
+    root_directory = str(Path(__file__).parent.parent)
+    holistic_detector = DetectorHandler(
+        pose=PoseDetector(root_directory + "/models/pose_landmarker_full.task"),
+        hand=HandDetector(root_directory + "/models/hand_landmarker.task"),
+        face=FaceDetector(root_directory + "/models/face_landmarker.task")
+    )
 
-  cap = cv2.VideoCapture(0)
-  image_provider = WebcamImageProvider(cache_queue_length=2, device_index=0)
-  while image_provider.is_opened:
-      # Break in key Ctrl+C pressed
-      if cv2.waitKey(5) & 0xFF == 27:
-          break
+    cap = cv2.VideoCapture(0)
+    image_provider = WebcamImageProvider(cache_queue_length=2, device_index=0)
+    while image_provider.is_opened:
+        # Break in key Ctrl+C pressed
+        if cv2.waitKey(5) & 0xFF == 27:
+            break
 
-      # Inference pose
-      image_provider.update()
-      image = image_provider.latest_frame
-      if image is None:
-          continue
-      holistic_detector.inference(image)
+        # Inference pose
+        image_provider.update()
+        image = image_provider.latest_frame
+        if image is None:
+            continue
+        holistic_detector.inference(image)
 
         # Filtering (Note: use copy.deepcopy if you need to filter results)
-      results = holistic_detector.results
+        results = holistic_detector.results
 
-      # Send results to solver app
-      packed_results = packer.pack_holistic_landmarks_result(results)
-      pose_sender.send_holistic_landmarks(packed_results)
+        # Send results to solver app
+        packed_results = packer.pack_holistic_landmarks_result(results)
+        pose_sender.send_holistic_landmarks(packed_results)
 
-      # Visualize resulted landmarks
-      annotated_image = image
-      if holistic_detector.results.pose_landmarks is not None:
-        annotated_image = visualizer.draw_pose_landmarks_on_image(annotated_image, results.pose_landmarks)
-      if results.hand_landmarks is not None:
-        annotated_image = visualizer.draw_hand_landmarks_on_image(annotated_image, results.hand_landmarks)
-      if results.face_results is not None:
-        annotated_image = visualizer.draw_face_landmarks_on_image(annotated_image, results.face_results)
-      cv2.imshow('MediaPipe Landmarks', cv2.flip(annotated_image, 1))
-      time.sleep(1/60)
-  cap.release()
+        # Visualize resulted landmarks
+        annotated_image = image
+        if holistic_detector.results.pose_landmarks is not None:
+            annotated_image = visualizer.draw_pose_landmarks_on_image(annotated_image, results.pose_landmarks)
+        if results.hand_landmarks is not None:
+            annotated_image = visualizer.draw_hand_landmarks_on_image(annotated_image, results.hand_landmarks)
+        if results.face_results is not None:
+            annotated_image = visualizer.draw_face_landmarks_on_image(annotated_image, results.face_results)
+        cv2.imshow('MediaPipe Landmarks', cv2.flip(annotated_image, 1))
+        time.sleep(1/60)
+    cap.release()
 
