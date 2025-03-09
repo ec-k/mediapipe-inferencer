6cc9ec9756f91a546c3b74db60f03eb2869d2ae1
EC-K <elxone.19jt@gmail.com>
2025-03-09 22:16:57 +0900

    feat(detector): adapt detectors to video inference

--- src/mediapipe_inferencer_core/detector/landmark_detector.py
+++ src/mediapipe_inferencer_core/detector/landmark_detector.py
@@ -1,4 +1,3 @@
-import mediapipe as mp
 from mediapipe.tasks import python as mp_tasks
 from abc import ABC, abstractproperty, abstractclassmethod
 
@@ -11,12 +10,18 @@ class LandmarkDetector(ABC):
         pass
 
 class PoseDetector(LandmarkDetector):
-    def __init__(self, model_path):
-        options = mp_tasks.vision.PoseLandmarkerOptions(
-            base_options = mp_tasks.BaseOptions(model_asset_buffer = open(model_path, "rb").read()),
-            running_mode = mp_tasks.vision.RunningMode.LIVE_STREAM,
-            result_callback = self.__save_results
-            )
+    def __init__(self, model_path, running_mode = mp_tasks.vision.RunningMode.LIVE_STREAM):
+        options = None
+        if running_mode == mp_tasks.vision.RunningMode.LIVE_STREAM:
+            options = mp_tasks.vision.PoseLandmarkerOptions(
+                base_options = mp_tasks.BaseOptions(model_asset_buffer = open(model_path, "rb").read()),
+                running_mode = running_mode,
+                result_callback = self.__save_results)
+        else:
+            options = mp_tasks.vision.PoseLandmarkerOptions(
+                base_options = mp_tasks.BaseOptions(model_asset_buffer = open(model_path, "rb").read()),
+                running_mode = running_mode)
+        self._running_mode = running_mode
         self.landmarker = mp_tasks.vision.PoseLandmarker.create_from_options(options)
         self.__results = None
 
@@ -28,17 +33,27 @@ class PoseDetector(LandmarkDetector):
         self.__results = results
 
     def inference(self, image, frame_timestamp_ms):
-        self.landmarker.detect_async(image, frame_timestamp_ms)
+        if self._running_mode == mp_tasks.vision.RunningMode.LIVE_STREAM:
+            self.landmarker.detect_async(image, frame_timestamp_ms)
+        elif self._running_mode == mp_tasks.vision.RunningMode.VIDEO:
+            self.__results = self.landmarker.detect_for_video(image, frame_timestamp_ms)
 
 
 class HandDetector(LandmarkDetector):
-    def __init__(self, model_path):
-        options = mp_tasks.vision.HandLandmarkerOptions(
-            base_options = mp_tasks.BaseOptions(model_asset_buffer = open(model_path, "rb").read()),
-            num_hands = 2,
-            running_mode = mp_tasks.vision.RunningMode.LIVE_STREAM,
-            result_callback=self.__save_results)
-
+    def __init__(self, model_path, running_mode = mp_tasks.vision.RunningMode.LIVE_STREAM):
+        options = None
+        if running_mode == mp_tasks.vision.RunningMode.LIVE_STREAM:
+            options = mp_tasks.vision.HandLandmarkerOptions(
+                base_options = mp_tasks.BaseOptions(model_asset_buffer = open(model_path, "rb").read()),
+                num_hands = 2,
+                running_mode = running_mode,
+                result_callback=self.__save_results)
+        else:
+            options = mp_tasks.vision.HandLandmarkerOptions(
+                base_options = mp_tasks.BaseOptions(model_asset_buffer = open(model_path, "rb").read()),
+                num_hands = 2,
+                running_mode = running_mode)
+        self._running_mode = running_mode
         self.landmarker = mp_tasks.vision.HandLandmarker.create_from_options(options)
         self.__results = None
 
@@ -50,19 +65,31 @@ class HandDetector(LandmarkDetector):
         self.__results = results
 
     def inference(self, image, frame_timestamp_ms):
-        self.landmarker.detect_async(image, frame_timestamp_ms)
+        if self._running_mode == mp_tasks.vision.RunningMode.LIVE_STREAM:
+            self.landmarker.detect_async(image, frame_timestamp_ms)
+        elif self._running_mode == mp_tasks.vision.RunningMode.VIDEO:
+            self.__results = self.landmarker.detect_for_video(image, frame_timestamp_ms)
 
 
 class FaceDetector(LandmarkDetector):
-    def __init__(self, model_path):
-        options = mp_tasks.vision.FaceLandmarkerOptions(
-            base_options = mp_tasks.BaseOptions(model_asset_buffer = open(model_path, "rb").read()),
-            output_face_blendshapes = True,
-            output_facial_transformation_matrixes = True,
-            num_faces = 1,
-            running_mode = mp_tasks.vision.RunningMode.LIVE_STREAM,
-            result_callback=self.__save_results)
-
+    def __init__(self, model_path, running_mode = mp_tasks.vision.RunningMode.LIVE_STREAM):
+        options = None
+        if running_mode == mp_tasks.vision.RunningMode.LIVE_STREAM:
+            options = mp_tasks.vision.FaceLandmarkerOptions(
+                base_options = mp_tasks.BaseOptions(model_asset_buffer = open(model_path, "rb").read()),
+                output_face_blendshapes = True,
+                output_facial_transformation_matrixes = True,
+                num_faces = 1,
+                running_mode = running_mode,
+                result_callback=self.__save_results)
+        else:
+            options = mp_tasks.vision.FaceLandmarkerOptions(
+                base_options = mp_tasks.BaseOptions(model_asset_buffer = open(model_path, "rb").read()),
+                output_face_blendshapes = True,
+                output_facial_transformation_matrixes = True,
+                num_faces = 1,
+                running_mode = running_mode)
+        self._running_mode = running_mode
         self.landmarker = mp_tasks.vision.FaceLandmarker.create_from_options(options)
         self.__results = None
 
@@ -74,5 +101,8 @@ class FaceDetector(LandmarkDetector):
         self.__results = results
 
     def inference(self, image, frame_timestamp_ms):
-        self.landmarker.detect_async(image, frame_timestamp_ms)
+        if self._running_mode == mp_tasks.vision.RunningMode.LIVE_STREAM:
+            self.landmarker.detect_async(image, frame_timestamp_ms)
+        elif self._running_mode == mp_tasks.vision.RunningMode.VIDEO:
+            self.__results = self.landmarker.detect_for_video(image, frame_timestamp_ms)
 
